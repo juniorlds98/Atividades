1c2f3e57ddbcabad3943327c72e3af19ff11a511
Junior <juniorlds.98@gmail.com>
2024-01-16 16:42:13 -0300

    Atividade 2

--- atividades/js/scripts.js
+++ atividades/js/scripts.js
@@ -4,10 +4,10 @@ const listaNaoOrdenada = document.querySelector('ul');
 const link = document.getElementById('link');
 var listaordenada = document.getElementById('#listaordenarda')
 
-titulo.innerText = 'Bem vindo ao mundo Tech';
-link.innerText = 'Link para ProZ Educação'
+titulo.innerText ='Bem vindo ao mundo Tech';
+link.innerText ='Link para ProZ Educação'
 
-listaNaoOrdenada.innerHTML = '<li>Onde pesquisar</li><li>Aprenda sobre JS</li><li>Sobre DOM</li>';
+listaNaoOrdenada.innerHTML ='<li>Onde pesquisar</li><li>Aprenda sobre JS</li><li>Sobre DOM</li>';
 
 listaordenada.innerHTML = 
 '<li><a www.google.com.br></a></li>'
